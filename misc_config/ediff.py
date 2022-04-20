36f560bf57c6365c33cd58174d737f778cc1c9a6
r.yang <r.yang@tianrang-inc.com>
2022-04-20 21:30:12 +0800

    feat: ediff log

--- misc_config/ediff.py
+++ misc_config/ediff.py
@@ -13,6 +13,6 @@ else:
     if os.sep == "/":  # windows
         file1 = file1.replace("\\", "/")
         file2 = file2.replace("\\", "/")
-        cmd = """emacs -nw -q --eval "(progn (setq ediff-split-window-function 'split-window-horizontally) (ediff-files \\"%s\\" \\"%s\\"))" """ % (
-            file1, file2)
+        # cmd = """emacsclient -c -nw --eval "(progn (setq ediff-split-window-function 'split-window-horizontally) (set-face-attribute 'diff-refine-added nil :foreground \\"white\\" :background \\"darkgreen\\") (set-face-attribute 'diff-refine-removed nil :foreground \\"white\\" :background \\"darkred\\") (set-face-attribute 'diff-refine-changed nil :foreground \\"white\\" :background \\"darkblue\\") (ediff-files \\"%s\\" \\"%s\\"))" """ % (file1, file2)
+        cmd = """emacs -nw -q --eval "(progn (setq ediff-split-window-function 'split-window-horizontally) (custom-set-faces '(ediff-current-diff-A ((t (:foreground \\"Black\\" :background \\"#7fd4b1\\")))) '(ediff-current-diff-B ((t (:foreground \\"Black\\" :background \\"#f7cc9e\\")))) '(ediff-even-diff-A ((t (:foreground \\"White\\" :background \\"#434154\\")))) '(ediff-even-diff-B ((t (:foreground \\"White\\" :background \\"#434154\\")))) '(ediff-odd-diff-A ((t (:foreground \\"White\\" :background \\"#434154\\")))) '(ediff-odd-diff-B ((t (:foreground \\"White\\" :background \\"#434154\\"))))) (ediff-files \\"%s\\" \\"%s\\"))" """ % (file1, file2)
         os.system(cmd)
